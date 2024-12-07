c0a5b84cf8335a2369c8ff0cb626247078722698
tomatyu <168719695+tomatyu@users.noreply.github.com>
2024-12-07 00:58:52 +0000

    あ

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,4 +2,6 @@ import streamlit as st
 
 prompt = st.chat_input("Say something")
 if prompt:
-    st.write(f"User has sent the following prompt: {prompt}")
+    st.write(f"あなたからの発言: {prompt}")
+if prompt:
+    st.write(f"トマト信者からの回答:トマトは最高にうまい")
