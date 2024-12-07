d39806869de16f4f1b01e0006bdc75ded5f3f0a2
tomatyu <168719695+tomatyu@users.noreply.github.com>
2024-12-07 00:49:23 +0000

    ｔ

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,18 +22,34 @@ def determine_winner(user, computer):
 # ストリームリットのUI部分
 st.title('じゃんけんゲーム')
 
-# ユーザーに手を選ばせる
-user_choice = st.radio('あなたの手を選んでください:', choices)
+# セッションステートの初期化
+if 'user_choice' not in st.session_state:
+    st.session_state.user_choice = None
+
+# ボタンを使って選択
+col1, col2, col3 = st.columns(3)
+
+with col1:
+    if st.button('グー'):
+        st.session_state.user_choice = 'グー'
+
+with col2:
+    if st.button('チョキ'):
+        st.session_state.user_choice = 'チョキ'
+
+with col3:
+    if st.button('パー'):
+        st.session_state.user_choice = 'パー'
 
 # ユーザーが手を選んだ後に即座に結果を表示
-if user_choice:
+if st.session_state.user_choice:
     # コンピュータの選択を取得
     comp_choice = computer_choice()
 
     # 勝敗判定
-    result = determine_winner(user_choice, comp_choice)
+    result = determine_winner(st.session_state.user_choice, comp_choice)
 
     # 結果を表示
-    st.write(f'あなたの手: {user_choice}')
+    st.write(f'あなたの手: {st.session_state.user_choice}')
     st.write(f'コンピュータの手: {comp_choice}')
     st.write(f'結果: {result}')
